b0d9f712abac4dcb96447739b00dcba28af94ec1
Gentlemannerss <damonvanbergen@gmail.com>
2023-04-19 10:54:53 +0200

    hotfix

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -6,34 +6,6 @@ function nameTv(arrayTV) {
 }
 nameTv(inventory);
 
-function name(banaanName){
-    return `${banaanName.brand} ${banaanName.type} - ${banaanName.name}`
-}
-
-function price(banaanPrice){
-    return `${banaanPrice.price}`
-}
-
-function sizes(banaanSizes){
-    return banaanSizes.map((screensize) => {
-        return `${screensize} inch (${screensize * 2.54} cm)}`
-    });
-}
-
-function All(AllTv){
-    const container = document.getElementById('test')
-    const tvList = AllTv.map((tv) => {
-        return `
-        <li>
-            <h3>${name(tv)}</h3>
-            <h4>${price(tv)}</h4> 
-            <h4>${sizes(tv.availableSizes)}</h4>
-        </li>
-`
-    })
-    container.innerHTML = `${tvList.join('')}`
-}
-All(inventory);
 
 /*const tvNames = nameTv(inventory);
 document.getElementById("nameTV").textContent = tvNames.join(", ");*/
@@ -86,7 +58,7 @@ function generateScreenSizes(tvList) {
 /* opdracht 4d*/
 
 function showInfo(tv) {
-    return `${nameTv(tv)}<br />${priceTV(tv)}<br />${generateScreenSizes(tv)}<br /><br \>`
+    return `${nameTv(tv)}${priceTV(tv)}${generateScreenSizes(tv)}`
 }
 
 
@@ -94,5 +66,38 @@ document.getElementById("allInfo").textContent = showInfo(inventory);
 
 /* Opdracht 4e*/
 function generateProductList(inventory) {
-    return inventory.map(tv => `${nameTv(tv)}<br />${priceTV(tv)}<br />${generateScreenSizes(tv)}<br /><br />`)
+    return inventory.map(tv => `${nameTv(tv)}${priceTV(tv)}${generateScreenSizes(tv)}`)
 }
+
+
+/* ----------------------- Uitwerking 2 ------------------------------*/
+
+
+function name(banaanName){
+    return `${banaanName.brand} ${banaanName.type} - ${banaanName.name}`
+}
+
+function price(banaanPrice){
+    return `${banaanPrice.price}`
+}
+
+function sizes(banaanSizes){
+    return banaanSizes.map((screensize) => {
+        return `${screensize} inch (${screensize * 2.54} cm)`
+    });
+}
+
+function All(AllTv){
+    const container = document.getElementById('test')
+    const tvList = AllTv.map((tv) => {
+        return `
+        <li>
+            <h3>${name(tv)}</h3>
+            <h4>${price(tv)}</h4> 
+            <h4>${sizes(tv.availableSizes)}</h4>
+        </li>
+    `
+    })
+    container.innerHTML = `${tvList.join('')}`
+}
+All(inventory);
